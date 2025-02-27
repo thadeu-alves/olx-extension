108d2bd6a8c51dab8cbc4280e31c99377ed712f4
thadeu-alves <thadeutelecentrosg@gmail.com>
2025-02-27 15:18:33 -0300

    get all prices and display

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function init(){
     const divSec = document.querySelector(".sc-67f3da08-0, .hDJPTR");
-    const secs = divSec.querySelector("section");
-    console.log(secs);
+    const secs = divSec.querySelectorAll("section");
+    const lista = document.createElement("ul");
+    lista.classList.add("thz-ul");
+    secs.forEach(sec => {
+        let li = document.createElement("li");
+        li.innerText = sec.querySelector("h3").innerText;
+        lista.appendChild(li);
+    });
+    divSec.insertAdjacentElement("beforebegin", lista);
 }
 
 init();
